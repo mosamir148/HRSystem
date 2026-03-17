d5980843de1ea389f04d21a2bc88fda6b3b7b0c5
mohamedsamir <msm318628@gmail.com>
2026-03-18 00:27:48 +0200

    remove last two file

--- HRSystem.API/HRSystem.API/Program.cs
+++ HRSystem.API/HRSystem.API/Program.cs
@@ -45,8 +45,7 @@ namespace HRSystem.API
 
             app.MapControllers();
 
-            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            app.Run($"http://0.0.0.0:{port}");
+            app.Run();
         }
     }
 }
